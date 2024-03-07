7c0eecceb763eca55a62b17c1dcbbfa5c0191b8c
Ducharme-Vanier <e2151578@cmaisonneuve.qc.ca>
2024-03-07 17:08:29 -0500

    Fini labo4, ajout du menu pour mobile

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/normalize.css';?>" >
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style.css';?>" >
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div id="menu" class="global">
+        <header class="entete__menu">
+            <input id="chk-burger" type="checkbox">
+                <label id="burger" for="chk-burger">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="#000"><path d="M0 0h24v24H0z" fill="none"></path><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>
+            </label>
+            <?php wp_nav_menu(array("container"=>"nav")); ?>
+        </header>
+    </div>
--- index.php
+++ index.php
@@ -1,38 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/normalize.css';?>" >
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style.css';?>" >
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@500&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-</head>
-<body>
-    <div id="menu" class="global">
-        <div class="entete__menu">
-            <?php wp_nav_menu(); ?>
-        </div>
-    </div>
+<?php
+    // Modèle de base index.php
+?>
+<?php
+    get_header();
+?>
     <div id="entete" class="global">
-        <header class="entete__header">
+        <section class="entete__header">
         <h1 class="bgc-texte">Thème du groupe #2 (h1)</h1>
         <h2 class="bgc-texte">4W4 - Conception interface <span> & développement Web</span>
             </h2>
         <h3 class="bgc-texte">Tim - Collège de Maisonneuve</h3>
         <button class="entete__button">Évènements</button>
         <link rel="stylesheet" href="style.css">
-         </header>
-         <div class="vague">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill:var(--couleur-arriere-acceuil)"></path>
-            </svg>
-        </div>
+        </section>
+ <?php
+    get_template_part("gabarit/vague");
+ ?>
 
     </div>
     <div id="acceuil" class="global ">
@@ -120,11 +103,9 @@
         <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, ullam sequi voluptates facilis necessitatibus quos tempore, magnam voluptate eveniet unde fuga nostrum optio repellat dolorem fugiat deserunt repellendus ad ea?</h6>
         
     </section>
-    <div class="vague">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill:var(--couleur-arriere-footer)"></path>
-        </svg>
-    </div>
+<?php
+    get_template_part("gabarit/vague");
+?>
 
 
     </div>
